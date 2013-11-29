7b4e8bb50921be67dcc9dac1bee61a3fb26dcbbc
laruekevin <larue.kev@gmail.com>
2013-11-29 18:44:20 +0100

    Ajout des activité dans map

--- src/fr/utt/erasmutt/maps/MapActivity.java
+++ src/fr/utt/erasmutt/maps/MapActivity.java
@@ -1,18 +1,12 @@
 package fr.utt.erasmutt.maps;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,78 +14,64 @@ import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import fr.utt.erasmutt.HomeActivity;
+import fr.utt.erasmutt.Constants;
 import fr.utt.erasmutt.R;
 
-public class MapActivity extends Activity {
-
-	private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
-    private ActionBarDrawerToggle mDrawerToggle;
-	
-    private CharSequence mDrawerTitle;
-    private CharSequence mTitle;
-    private String[] mTitles;
-    
+public class MapActivity extends Activity implements LocationListener{
+	LatLng myPosition;
+	LatLng positionResto1;
+	GoogleMap map;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
-		
-        mTitle = mDrawerTitle = getTitle();
-        mTitles = getResources().getStringArray(R.array.text_menu_array);
-		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-        // set a custom shadow that overlays the main content when the drawer opens
-        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-        // set up the drawer's list view with items and click listener
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, mTitles));
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        // Getting GoogleMap object from the fragment
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 
-        // enable ActionBar app icon to behave as action to toggle nav drawer
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-        getActionBar().setHomeButtonEnabled(true);
-        //Set a blue Background
-        getActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.background_blue)));
+        // Enabling MyLocation Layer of Google Map
+		map.setMyLocationEnabled(true);
 
-        // ActionBarDrawerToggle ties together the the proper interactions
-        // between the sliding drawer and the action bar app icon
-        mDrawerToggle = new ActionBarDrawerToggle(
-                this,                  /* host Activity */
-                mDrawerLayout,         /* DrawerLayout object */
-                R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
-                R.string.drawer_open,  /* "open drawer" description for accessibility */
-                R.string.drawer_close  /* "close drawer" description for accessibility */
-                ) {
-            public void onDrawerClosed(View view) {
-                getActionBar().setTitle(mTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
+        // Getting LocationManager object from System Service LOCATION_SERVICE
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-            public void onDrawerOpened(View drawerView) {
-                getActionBar().setTitle(mDrawerTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-        };
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        // Creating a criteria object to retrieve provider
+        Criteria criteria = new Criteria();
 
-		
-        // Get a handle to the Map Fragment
-        GoogleMap map = ((MapFragment) getFragmentManager()
-                .findFragmentById(R.id.map)).getMap();
+        // Getting the name of the best provider
+        String provider = locationManager.getBestProvider(criteria, true);
 
-        LatLng sydney = new LatLng(-33.867, 151.206);
+        // Getting Current Location
+        Location location = locationManager.getLastKnownLocation(provider);
 
-        map.setMyLocationEnabled(true);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
-
-        map.addMarker(new MarkerOptions()
-                .title("Sydney")
-                .snippet("The most populous city in Australia.")
-                .position(sydney));
+        if(location!=null){
+	        // Getting latitude of the current location
+	        double latitude = location.getLatitude();
+	
+	        // Getting longitude of the current location
+	        double longitude = location.getLongitude();
+	
+	        // Creating a LatLng object for the current location
+	
+	         myPosition = new LatLng(latitude, longitude);
+	         
+	         map.moveCamera(CameraUpdateFactory.newLatLngZoom(myPosition, 13));
+	         
+	         addActivitiesCloseToUser();
+	         
+        }
 
 	}
+	public void addActivitiesCloseToUser(){
+		
+		for(int i=0 ; i<Constants.tabActivityForUser.size() ; i++) { 
+			String name = Constants.tabActivityForUser.get(i).getName();
+			LatLng position= new LatLng(Double.parseDouble(Constants.tabActivityForUser.get(i).getLatitude()),Double.parseDouble(Constants.tabActivityForUser.get(i).getLongitude()));
+			map.addMarker(new MarkerOptions().position(position).title(name));
+		}
+		
+	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -99,66 +79,32 @@ public class MapActivity extends Activity {
 		getMenuInflater().inflate(R.menu.map, menu);
 		return true;
 	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
 	
-    /* Called whenever we call invalidateOptionsMenu() */
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the nav drawer is open, hide action items related to the content view
-        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
-        menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
-        return super.onPrepareOptionsMenu(menu);
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-         // The action bar home/up action should open or close the drawer.
-         // ActionBarDrawerToggle will take care of this.
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        // Handle action buttons
-        switch(item.getItemId()) {
-        
-        case R.id.action_location:
-            return true;
-        
-        case R.id.action_settings:
-            return true;
-        default:
-            return super.onOptionsItemSelected(item);
-        }
-    }
-    
-    /* The click listner for ListView in the navigation drawer */
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position);
-        }
-    }
-    
-    private void selectItem(int position) {
-        // update the main content by replacing fragments
-    	//TODO : À changer pour switcher avec des fragments multiple
-		Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-    	switch (position) {
-		case 0:
-			startActivity(intent);
-			break;
-		case 1:
-			startActivity(intent);
-			break;
-		case 2:
-			startActivity(intent);
-			break;
-		case 3:
-			startActivity(intent);
-			break;	
 
-		default:
-			break;
-		}
-    	
-    }
 
+	
 }
